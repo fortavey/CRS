647a3af117bc4d0edf14d4b0819fc1682fb77991
fortavey <fortavey@gmail.com>
2021-09-03 19:26:38 +0300

    Add google tag manager

--- CryptoSignal/src/main.js
+++ CryptoSignal/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
+import VueGtag from 'vue-gtag';
 import App from './App.vue';
 import store from './store';
 
-createApp(App).use(store).mount('#app');
+createApp(App).use(store).use(VueGtag, { config: { id: 'G-Y7ZLZ5V941' } }).mount('#app');
