c8de1b603119017c20d165d0d3e33978adc50051
fortavey <fortavey@gmail.com>
2021-09-07 18:29:22 +0300

    add log files

---
+++ forwardMessage.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function(bot, msg, userList){
+  const html = `${msg.text}`;
+        
+  userList.forEach(user => {
+      bot.forwardMessage(user, msg.chat.id, msg.message_id)
+        .then(data => console.log(`Send: ${user}`))
+        .catch(err => {
+            const stamp = Date.now();
+            fs.writeFile(path.resolve(__dirname, `log/${stamp}.txt`), JSON.stringify(err), function (error) {
+                if (error) return console.log(error);
+                console.log(`ERROR: ${user} - write to file /log/${stamp}.txt`);
+            });
+        });
+  });
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const userList = require('./userList');
 const textObj = require('./text');
 const TOKEN = require('./token');
+const myForwardMessage = require('./forwardMessage');
 
 
 const bot = new TelegramBot(TOKEN, {
@@ -9,18 +10,14 @@ const bot = new TelegramBot(TOKEN, {
 });
 
 bot.on('message', (msg) => {
+    let str = textObj.str;
 
     if(msg.from.id == '1880184913') {
-        const html = `${msg.text}`;
-        
-        userList.forEach(user => {
-            bot.forwardMessage(user, msg.chat.id, msg.message_id);
-        });
-        
+        myForwardMessage(bot, msg, userList);
     }else {
 
         if( userList.includes(msg.from.id) ) {
-            textObj.str = textObj.str2;
+            str = textObj.str2;
         }
 
         const isStar = msg.from.first_name.includes('*'); // Звёздочка в имени пользователя
@@ -28,7 +25,7 @@ bot.on('message', (msg) => {
 
         const html = `*Здравствуйте, ${name}
             
-            ${textObj.str}
+            ${str}
         `;
         bot.sendMessage(msg.chat.id, html, {
             parse_mode: 'Markdown'
