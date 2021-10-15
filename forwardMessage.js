53d7dd0ae22b87111c0693dafad7eddba1b0d8a5
fortavey <fortavey@gmail.com>
2021-10-15 16:35:21 +0300

    change log

--- forwardMessage.js
+++ forwardMessage.js
@@ -1,18 +1,26 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-module.exports = function(bot, msg, userList){
-  const html = `${msg.text}`;
-        
-  userList.forEach(user => {
-      bot.forwardMessage(user, msg.chat.id, msg.message_id)
-        .then(data => console.log(`Send: ${user}`))
-        .catch(err => {
-            const stamp = Date.now();
-            fs.writeFile(path.resolve(__dirname, `log/${stamp}.txt`), JSON.stringify(err), function (error) {
-                if (error) return console.log(error);
-                console.log(`ERROR: ${user} - write to file /log/${stamp}.txt`);
-            });
-        });
-  });
+module.exports = function (bot, msg, userList) {
+  const html = `${msg.text}`
+
+  userList.forEach((user) => {
+    bot
+      .forwardMessage(user, msg.chat.id, msg.message_id)
+      .then((data) => console.log(`Send: ${user}`))
+      .catch((err) => {
+        const stamp = Date.now()
+        fs.writeFile(
+          path.resolve(__dirname, `log/${stamp}.txt`),
+          JSON.stringify({
+            err: `ERROR: ${user} - write to file /log/${stamp}.txt`,
+            error: err,
+          }),
+          function (error) {
+            if (error) return console.log(error)
+            console.log(`ERROR: ${user} - write to file /log/${stamp}.txt`)
+          }
+        )
+      })
+  })
 }
